3ea81df3ba2c81d2e2498e6e52f7a453e9f5d59e
Kurják Richárd <kurjak.richard@gmail.com>
2021-03-27 03:55:09 +0100

    Add fakeMain to build artifact

---
+++ src/com/sire/librarymanagement/fakeMain.java
@@ -0,0 +1,9 @@
+package com.sire.librarymanagement;
+
+import com.sire.librarymanagement.MainLoader;
+
+public class fakeMain {
+    public static void main(String[] args) {
+        MainLoader.main(args);
+    }
+}
